c513942dec40f4449fd5626aba95ef4ab5849ea9
KSYong <ericyong95@naver.com>
2022-10-05 18:59:50 +0900

    Initial Commit

---
+++ allen_hackathon_level2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  allen_hackathon_level2
+//
+//  Created by 권승용 on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
