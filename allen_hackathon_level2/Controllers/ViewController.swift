db070ac986055327cf29d52e37bd33f9a1d2cb32
KSYong <ericyong95@naver.com>
2022-10-06 11:14:18 +0900

    feat: 기본 UI 배치 및 IBOutlet 연결

--- allen_hackathon_level2/Controllers/ViewController.swift
+++ allen_hackathon_level2/Controllers/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var currentLocationLabel: UILabel!
+    
+    @IBOutlet weak var grantLocationPermissionButton: UIButton!
+    
+    @IBOutlet weak var searchLocationButton: UIButton!
+    
+    @IBOutlet weak var locationMapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.requestWhenInUseAuthorization()
     }
 
-
+    @IBAction func grantLocationButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func searchLocationButtonTapped(_ sender: Any) {
+    }
+    
 }
 
